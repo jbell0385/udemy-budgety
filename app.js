03739c325f666e537075c059c90c7a625023f048
Jeremy Bell <jbell0358@gmail.com>
2017-07-15 14:52:52 -0700

    Added updateBudget() functionality.

--- app.js
+++ app.js
@@ -13,6 +13,14 @@
       this.value = value;
     };
 
+    var calculateTotal = function(type) {
+      var sum = 0;
+      data.allItems[type].forEach(function(cur) {
+        sum += cur.value;
+      });
+      data.totals[type] = sum;
+    }
+
     var data = {
       allItems: {
         exp: [],
@@ -21,7 +29,9 @@
       totals: {
         exp: 0,
         inc: 0
-      }
+      },
+      budget: 0,
+      percentage: -1
     }
 
     return {
@@ -48,7 +58,34 @@
         //return
         return newItem;
 
+      },
+
+      calculateBudget: function() {
+        //calculate total income and expenses
+        calculateTotal('exp');
+        calculateTotal('inc');
+
+        //calculate the budget: income - expenses
+        data.budget = data.totals.inc - data.totals.exp;
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+
+        //calculate the percentage of income that we spent
+
+      },
+
+      getBudget: function() {
+        return {
+          budget: data.budget,
+          totalInc: data.totals.inc,
+          totalExp: data.totals.exp,
+          percentage: data.percentage
+        }
+      },
+
+      testing: function() {
+        console.log(data);
       }
+
     }
 
   })();
@@ -62,8 +99,8 @@
       inputDescription: '.add__description',
       inputValue: '.add__value',
       inputBtn: '.add__btn',
-      incomeContainer:'.income__list',
-      expensesContainer:'.expenses__list'
+      incomeContainer: '.income__list',
+      expensesContainer: '.expenses__list'
     }
 
     return {
@@ -71,7 +108,7 @@
         return {
           type: document.querySelector(DOMstrings.inputType).value, //Will be either inc or exp
           description: document.querySelector(DOMstrings.inputDescription).value,
-          value: document.querySelector(DOMstrings.inputValue).value
+          value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
         }
       },
       addListItem: function(obj, type) {
@@ -97,12 +134,25 @@
         newHtml = newHtml.replace('%value%', obj.value);
 
         //Insert the HTML into the DOM
-        document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
+        document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+      },
+
+      clearFields: function() {
+        var fields;
+        fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+        var fieldsArr = Array.prototype.slice.call(fields);
+
+        fieldsArr.forEach(function(current, index, array) {
+          current.value = "";
+        });
+        fieldsArr[0].focus();
       },
 
       getDOMStrings: function() {
         return DOMstrings;
       }
+
     }
   })();
 
@@ -123,6 +173,16 @@
       });
     };
 
+    var updateBudget = function() {
+      //1. Calculate the budget
+      budgetCtrl.calculateBudget();
+
+      //2. return the budget
+      var budget = budgetCtrl.getBudget();
+
+      //3. Display the budget on the UI
+      console.log(budget);
+    }
 
     var ctrlAddItem = function() {
       var input, newItem;
@@ -130,14 +190,20 @@
       //1. Get the field input data
       input = UICtrl.getinput();
 
-      //2. Add the item to the budget controller
-      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+      if (input.description !== "" && !(isNaN(input.value)) && input.value > 0) {
+        //2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-      //3. Add the new item to the UI
-      UICtrl.addListItem(newItem,input.type);
-      //4. Calculate the budget
+        //3. Add the new item to the UI
+        UICtrl.addListItem(newItem, input.type);
 
-      //5. Display the budget on the UI
+        //4. Clear the fields
+        UICtrl.clearFields();
+
+        //5.Calculate and update budget
+        updateBudget();
+      }
+
     }
     return {
       init: function() {
